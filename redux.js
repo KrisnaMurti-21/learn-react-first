d2d4706545be9ed4903d3565642b698a5bc151b8
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-03-03 09:52:58 +0700

    redux learn

--- redux.js
+++ redux.js
@@ -1,7 +1,13 @@
 // reducer
 
+// const { createStore } = require("redux");
+import { legacy_createStore } from "redux";
+
 const cartReducer = ( state = {
-    cart: []
+    cart: [{
+        id: 1,
+        qty: 20
+    }]
 }, action
 ) => {
     switch (action.type) {
@@ -10,15 +16,30 @@ const cartReducer = ( state = {
                 ...state,
                 cart: [...state.cart, action.payload]
             }
-            
+
             break;
-    
+
         default:
-            break;
+            return state;
     }
 }
 //store
+const store = legacy_createStore(cartReducer)
+console.log("oncreate store : ",store.getState())
+
 
 //Subscribe
+store.subscribe(() => {
+    console.log("store changed : ", store.getState())
+})
 
 //dispatch
+const action1 = {
+    type: "ADD_TO_CART",
+    payload: {
+        id: 2,
+        qty: 10
+    }
+}
+
+store.dispatch(action1)
